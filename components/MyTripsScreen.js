977ccbebe8475be892e2b5c9ff84820c53e35d03
MadeleineGustafson <madeleinegu@gmail.com>
2023-12-21 14:13:00 +0100

    added input fieds and text

--- components/MyTripsScreen.js
+++ components/MyTripsScreen.js
@@ -1,11 +1,11 @@
-import { Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 function MyTripsScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text> My Trips Screen</Text>
     </View>
-   );
- }
- 
- export default MyTripsScreen
+  );
+}
+
+export default MyTripsScreen;
--- components/NewTripScreen.js
+++ components/NewTripScreen.js
@@ -1,11 +1,45 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 function NewTripScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text> New trip Screen</Text>
+      <SafeAreaView>
+        <Text> What is your name?</Text>
+        <TextInput
+          style={styles.input}
+          //onChangeText={onChangeNumber}
+          //value={number}
+          placeholder="Enter your Name"
+          //keyboardType="numeric"
+        />
+        <Text> Where are you going?</Text>
+        <TextInput
+          style={styles.input}
+          // onChangeText={onChangeNumber}
+          //value={number}
+          placeholder="Enter your destination"
+          keyboardType="numeric"
+        />
+        <Text> When are you going?</Text>
+        <TextInput
+          style={styles.input}
+          // onChangeText={onChangeNumber}
+          //value={number}
+          placeholder="Enter your travel dates"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+  },
+});
 
-export default NewTripScreen
+export default NewTripScreen;
