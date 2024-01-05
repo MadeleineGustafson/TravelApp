a98075e5d05a0b1229a72b2d15040452f51ce127
MadeleineGustafson <madeleinegu@gmail.com>
2024-01-05 15:01:35 +0100

    added todo

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { React } from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
 import { Calendar } from "react-native-calendars";
 
 function CalendarScreen() {
+  const navigation = useNavigation();
+  const handleAddTodoPress = () => {
+    // Navigate to the TodoPage when the "Add Todo" button is pressed
+    navigation.navigate("TodoPage");
+  };
   const today = new Date(); // Get today's date
   const dateString = today.toISOString().split("T")[0]; // Format the date as 'YYYY-MM-DD'
 
@@ -14,38 +20,42 @@ function CalendarScreen() {
   };
 
   return (
-    <Calendar
-      style={{
-        marginTop: 50,
-      }}
-      // Callback that gets called when the user selects a day
-      onDayPress={(day) => {
-        console.log("selected day", day);
-      }}
-      // Custom render function for each day
-      renderDay={renderDay}
-      // Other calendar configurations...
-
-      markingType={"period"}
-      markedDates={{
-        "2024-01-15": { marked: true, dotColor: "purple" },
-        "2024-01-16": { marked: true, dotColor: "purple" },
-        "2024-01-21": {
-          startingDay: true,
-          color: "#50cebb",
-          textColor: "white",
-        },
-        "2024-01-22": { color: "#70d7c7", textColor: "white" },
-        "2024-01-23": {
-          color: "#70d7c7",
-          textColor: "white",
-          marked: true,
-          dotColor: "purple",
-        },
-        "2024-01-24": { color: "#70d7c7", textColor: "white" },
-        "2024-01-25": { endingDay: true, color: "#50cebb", textColor: "white" },
-      }}
-    />
+    <>
+      <Calendar
+        style={{
+          marginTop: 50,
+        }}
+        // Callback that gets called when the user selects a day
+        onDayPress={(day) => {
+          console.log("selected day", day);
+        }}
+        // Custom render function for each day
+        renderDay={renderDay}
+        // Other calendar configurations...
+        markingType={"period"}
+        markedDates={{
+          "2024-01-15": { marked: true, dotColor: "purple" },
+          "2024-01-16": { marked: true, dotColor: "purple" },
+          "2024-01-21": {
+            startingDay: true,
+            color: "green",
+            textColor: "white",
+          },
+          "2024-01-22": { color: "green", textColor: "white" },
+          "2024-01-23": {
+            color: "green",
+            textColor: "white",
+            marked: true,
+            dotColor: "purple",
+          },
+          "2024-01-24": { color: "green", textColor: "white" },
+          "2024-01-25": { endingDay: true, color: "green", textColor: "white" },
+        }}
+      />
+      <Pressable onPress={handleAddTodoPress}>
+        <Text style={styles.addButton}>Add Todo</Text>
+      </Pressable>
+    </>
   );
 }
 
