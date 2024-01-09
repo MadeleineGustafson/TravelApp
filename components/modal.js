9b2a402ac573e6a7a81ea8c149e0dabdce325e0f
Jenny Weijland <jennyweij@outlook.com>
2024-01-09 19:23:10 +0100

    out commented the modal component for future delete

--- components/modal.js
+++ components/modal.js
@@ -1,49 +1,49 @@
-import { Button, Center, FormControl, Input, Modal } from "native-base";
-import React, { useState } from "react";
+// import { Button, Center, FormControl, Input, Modal } from "native-base";
+// import React, { useState } from "react";
 
-function TodoModal({ isVisible }) {
-  const [showModal, setShowModal] = useState(isVisible);
+// function TodoModal({ isVisible }) {
+//   const [showModal, setShowModal] = useState(isVisible);
 
-  return (
-    <Center>
-      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-        <Modal.Content maxWidth="400px">
-          <Modal.CloseButton />
-          <Modal.Header>Add Todo</Modal.Header>
-          <Modal.Body>
-            <FormControl>
-              <FormControl.Label>New todo</FormControl.Label>
-              <Input />
-            </FormControl>
-            <FormControl mt="3">
-              <FormControl.Label>Date</FormControl.Label>
-              <Input />
-            </FormControl>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button.Group space={2}>
-              <Button
-                variant="ghost"
-                colorScheme="blueGray"
-                onPress={() => {
-                  setShowModal(false);
-                }}
-              >
-                Cancel
-              </Button>
-              <Button
-                onPress={() => {
-                  setShowModal(false);
-                }}
-              >
-                Add Todo
-              </Button>
-            </Button.Group>
-          </Modal.Footer>
-        </Modal.Content>
-      </Modal>
-    </Center>
-  );
-}
+//   return (
+//     <Center>
+//       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+//         <Modal.Content maxWidth="400px">
+//           <Modal.CloseButton />
+//           <Modal.Header>Add Todo</Modal.Header>
+//           <Modal.Body>
+//             <FormControl>
+//               <FormControl.Label>New todo</FormControl.Label>
+//               <Input />
+//             </FormControl>
+//             <FormControl mt="3">
+//               <FormControl.Label>Date</FormControl.Label>
+//               <Input />
+//             </FormControl>
+//           </Modal.Body>
+//           <Modal.Footer>
+//             <Button.Group space={2}>
+//               <Button
+//                 variant="ghost"
+//                 colorScheme="blueGray"
+//                 onPress={() => {
+//                   setShowModal(false);
+//                 }}
+//               >
+//                 Cancel
+//               </Button>
+//               <Button
+//                 onPress={() => {
+//                   setShowModal(false);
+//                 }}
+//               >
+//                 Add Todo
+//               </Button>
+//             </Button.Group>
+//           </Modal.Footer>
+//         </Modal.Content>
+//       </Modal>
+//     </Center>
+//   );
+// }
 
-export default TodoModal;
+// export default TodoModal;
